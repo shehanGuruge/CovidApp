a8b9357c027736c4f44f4c39c2575f4f92630df9
MalithPanda <malith.fernando@revport.net>
2020-07-07 03:04:07 +0800

    update firebase details

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,8 @@
 import ShopItem from './myShopScreen/ShopItem';
 import CheckInView from './homeScreen/CheckInView';
 import Loader from './loader/Loader'
-import DialogBox from './dialogs/Alert'
 import {ShopCheckingItem} from './shopChecking/ShopCheckingItem';
 import {BackButton} from './back/BackButton';
 import LogOut from './logout/Logout'
 
-export {ShopItem,CheckInView,Loader,DialogBox,ShopCheckingItem,BackButton,LogOut} 
+export {ShopItem,CheckInView,Loader,ShopCheckingItem,BackButton,LogOut} 
